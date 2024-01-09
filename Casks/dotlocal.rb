816874bc7797945c85bfbe4293c94d92022a7194
msp5382 <meen.sankaew@gmail.com>
2024-01-09 22:00:10 +0700

    fix: filename

--- Casks/dotlocal.rb
+++ Casks/dotlocal.rb
@@ -1,7 +1,7 @@
 cask "dotlocal" do
     desc ".local domains for anything, powered by Orbstack"
     homepage "https://github.com/softnetics/dotlocal"
-    url "https://github.com/softnetics/dotlocal/releases/download/v0.1/DotLocal.0.1.dmg"
+    url "https://github.com/softnetics/dotlocal/releases/download/v0.1/DotLocal-v0.1.dmg"
     name "DotLocal"
     sha256 "55b5acff6eccc1503d35f446e3757248165e6541e736551d7e551d1ed7ecbb13"
     license "MIT"
