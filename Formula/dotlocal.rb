25c8943e35bbd2a22133f05b7e1f6975dd8c3170
msp5382 <meen.sankaew@gmail.com>
2024-01-09 21:40:15 +0700

    feat: init

---
+++ Formula/dotlocal.rb
@@ -0,0 +1,11 @@
+class DotLocal < Formula
+    desc ".local domains for anything, powered by Orbstack"
+    homepage "https://github.com/softnetics/dotlocal"
+    url "https://github.com/softnetics/dotlocal/releases/download/v0.1/DotLocal.0.1.dmg"
+    name "DotLocal"
+    sha256 "55b5acff6eccc1503d35f446e3757248165e6541e736551d7e551d1ed7ecbb13"
+    license "MIT"
+    version "0.1"
+
+    app "DotLocal.app"
+  end
