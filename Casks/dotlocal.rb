2c14e08750ef37800d82173adb8c1b917591eb54
Suphon T <8080853+suphon-t@users.noreply.github.com>
2024-01-11 11:22:04 +0700

    chore: set current version to 0.0.0

--- Casks/dotlocal.rb
+++ Casks/dotlocal.rb
@@ -1,7 +1,7 @@
 cask "dotlocal" do
     desc ".local domains for anything, powered by Orbstack"
     homepage "https://github.com/softnetics/dotlocal"
-    version "0.1"
+    version "0.0.0"
     url "https://github.com/softnetics/dotlocal/releases/download/v#{version}/DotLocal-v#{version}.dmg"
     name "DotLocal"
     sha256 "55b5acff6eccc1503d35f446e3757248165e6541e736551d7e551d1ed7ecbb13"
