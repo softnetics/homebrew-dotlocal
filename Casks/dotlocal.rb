495d4a29893645372869875a9f2198a47d3c3750
Suphon T <8080853+suphon-t@users.noreply.github.com>
2024-01-11 11:16:14 +0700

    fix: use interpolation in url

--- Casks/dotlocal.rb
+++ Casks/dotlocal.rb
@@ -1,7 +1,7 @@
 cask "dotlocal" do
     desc ".local domains for anything, powered by Orbstack"
     homepage "https://github.com/softnetics/dotlocal"
-    url "https://github.com/softnetics/dotlocal/releases/download/v0.1/DotLocal-v0.1.dmg"
+    url "https://github.com/softnetics/dotlocal/releases/download/v#{version}/DotLocal-v#{version}.dmg"
     name "DotLocal"
     sha256 "55b5acff6eccc1503d35f446e3757248165e6541e736551d7e551d1ed7ecbb13"
     version "0.1"
