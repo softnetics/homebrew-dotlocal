f4528724ef148eba856130c3e64370b33d278472
Suphon T <8080853+suphon-t@users.noreply.github.com>
2024-01-11 11:07:09 +0700

    chore: remove license field

--- Casks/dotlocal.rb
+++ Casks/dotlocal.rb
@@ -4,7 +4,6 @@ cask "dotlocal" do
     url "https://github.com/softnetics/dotlocal/releases/download/v0.1/DotLocal-v0.1.dmg"
     name "DotLocal"
     sha256 "55b5acff6eccc1503d35f446e3757248165e6541e736551d7e551d1ed7ecbb13"
-    license "MIT"
     version "0.1"
 
     app "DotLocal.app"
