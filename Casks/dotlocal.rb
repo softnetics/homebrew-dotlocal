f6d76ce485700e75a764462a924bafea4b3a148f
Suphon T <8080853+suphon-t@users.noreply.github.com>
2024-01-21 14:08:02 +0700

    dotlocal 0.1.0 (#2)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/dotlocal.rb
+++ Casks/dotlocal.rb
@@ -1,6 +1,6 @@
 cask "dotlocal" do
-  version "0.0.1"
-  sha256 "cae249804541f61ed453e6d4932a9c5f43064180fb0df48af50a8c49357c55c9"
+  version "0.1.0"
+  sha256 "3bf2b592e2a1f96fcb685cc542f3d59714814413a2f654d31e88c0f0feca5548"
 
   url "https://github.com/softnetics/dotlocal/releases/download/v#{version}/DotLocal-v#{version}.dmg"
   name "DotLocal"
